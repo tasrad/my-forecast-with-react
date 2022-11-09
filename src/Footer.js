3fcfd1f95232ec005faa86a7d4c6c172b6bc483c
tasrad <108927043+tasrad@users.noreply.github.com>
2022-11-09 14:19:39 +0200

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@
 export default function Footer(){
     return(
         <div className="Footer p-3 ps-0">
-        <a href="https://github.com/tasrad/my-forecast-with-react" target="_blank">   Open-source code </a> 
+        <a href="https://github.com/tasrad/my-forecast-with-react" target="_blank" rel="noreferrer">   Open-source code </a> 
              by Tatiana Mytsianka
         </div>
     )
