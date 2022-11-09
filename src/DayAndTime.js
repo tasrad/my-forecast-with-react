dfa847c7b58853c2ba335c5810b18678619555e6
tasrad <108927043+tasrad@users.noreply.github.com>
2022-11-09 15:57:19 +0200

    add days and time +css

--- src/DayAndTime.js
+++ src/DayAndTime.js
@@ -2,30 +2,30 @@ import React from 'react'
 
 export default function DayAndTime(props){
 
-    // let days = [
-    //     "Sunday",
-    //     "Monday",
-    //     "Tuersday",
-    //     "Wednesday",
-    //     "Thusday",
-    //     "Friday",
-    //     "Saturday"
-    //   ];
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuersday",
+        "Wednesday",
+        "Thusday",
+        "Friday",
+        "Saturday"
+      ];
 
-    //   let day = days[props.date.getDay()];
-    //   let hours = props.date.getHours();
-    //   if (hours <10){
-    //     hours = `0${hours}`
-    //   }
-    //   let minutes = props.date.getMinutes();
-    //   if (minutes <10){
-    //     minutes = `0${minutes}`
-    //   }
+      let day = days[props.date.getDay()];
+      let hours = props.date.getHours();
+      if (hours <10){
+        hours = `0${hours}`
+      }
+      let minutes = props.date.getMinutes();
+      if (minutes <10){
+        minutes = `0${minutes}`
+      }
 
 
     return(
         <div className="DayAndTime">
-            {/* {day}  {hours}:{minutes} */}
+            {day}  {hours}:{minutes}
         </div>
     );
 }
