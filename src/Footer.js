cb516de02e7adb8810299e85710d25678649031b
tasrad <108927043+tasrad@users.noreply.github.com>
2022-11-10 22:07:17 +0200

    changed footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,8 +3,9 @@
 export default function Footer(){
     return(
         <div className="Footer p-3 ps-0">
-        <a href="https://github.com/tasrad/my-forecast-with-react" target="_blank" rel="noreferrer">   Open-source code </a> 
-             by Tatiana Mytsianka
+            This project was coded by Tatiana Mytsianka and is
+        <a href="https://github.com/tasrad/my-forecast-with-react" target="_blank" rel="noreferrer">  open-sourced on GitHub </a> hosted on Netlify
+             
         </div>
     )
 }
