c252dfb06b7e94352d8ccb18bfef67093e82f2af
tasrad <108927043+tasrad@users.noreply.github.com>
2022-11-13 23:25:38 +0200

    update forecast

--- src/WeatherForWeek.js
+++ src/WeatherForWeek.js
@@ -1,19 +1,20 @@
-
+import React, { useState } from "react";
 import "./WeatherForWeek.css";
 import WeatherIcon from "./WeatherIcon.js";
 import axios from "axios";
 
 export default function WeatherForWeek(props){
-    console.log(props.coord.lon);
- function showResponse (props){
-    
+    const [forecast, setForecast] = useState("");
+console.log(forecast)
+ function showResponse (response){
+    setForecast(response.data.daily);
+    console.log(response.data.daily);
  }
 
 let lat = props.coord.lat;
 let lon = props.coord.lon;
 let key = "0ebc654fccbc00189d5408f3d6f15b08";
-let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}`;
-
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=metric`;
 axios.get(apiUrl).then(showResponse);
 
 
@@ -22,11 +23,17 @@ axios.get(apiUrl).then(showResponse);
             <div className="container">
                 <div className="row m-3">
                     <div className="col-2 border-0">
-                        <span>Mon</span>
+                        {/* <span>{forecast[0].dt*1000}</span> */}
+                        <div>
                         <WeatherIcon code={props.data.icon} size={29}/>
-                        <span className="forecast-temp">1°</span>
+                        </div>
+                        <span className="forecast-temp">
+                            {/* {Math.round(forecast[0].temp.max)}° */}
+                            </span>
                         <br />
-                        <span className="forecast-temp">-5°</span>
+                        <span className="forecast-temp">
+                            {/* {Math.round(forecast[0].temp.min)}° */}
+                            </span>
                     </div>
                     
                 </div>
