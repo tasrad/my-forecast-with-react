2d61459fbf4e33b8c9b2eef316897cf786c59565
tasrad <108927043+tasrad@users.noreply.github.com>
2022-11-14 23:27:31 +0200

    Update WeatherForWeek.js

--- src/WeatherForWeek.js
+++ src/WeatherForWeek.js
@@ -10,30 +10,26 @@ export default function WeatherForWeek(props){
     const [dayTemp, setDayTemp] = useState("");
     const [nightTemp, setNightTemp] = useState("");
 
-
-function showDay(){
-    let date = new Date(forecast[0].dt);
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    let day = date.getDay();
-    let forecastDay = days[day];
-
-   return forecastDay;
-}
-
-
  function showResponse (response){
  setForecast(response.data.daily);
- setDayTemp(response.data.daily.temp.day);
- setNightTemp(response.data.daily.temp.night);
+ setDayTemp(response.data.daily[0].temp.day);
+ setNightTemp(response.data.daily[0].temp.night);
  };
-
 
 let lat = props.coord.lat;
 let lon = props.coord.lon;
-let key = "25fad9f7e87157d33dde0f82ab269ee8";
+let key = "57821c3b75b60c68ecd1a8d0dd1aa8d3";
 let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=metric`;
 axios.get(apiUrl).then(showResponse);
 
+
+function showDay(){
+    let date = new Date(forecast[0].dt*1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let day = date.getDay();
+    let forecastDay = days[day];
+   return forecastDay;
+}
 
     return(
         <div className="WeatherForWeek ">
