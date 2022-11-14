fced037191e13bf5ba1f9d81e22aec3bb0b6d9a7
tasrad <108927043+tasrad@users.noreply.github.com>
2022-11-14 15:49:31 +0200

    Update WeatherForWeek.js

--- src/WeatherForWeek.js
+++ src/WeatherForWeek.js
@@ -1,26 +1,27 @@
 import React
-// , { useState } 
+, { useState } 
 from "react";
 import "./WeatherForWeek.css";
 import WeatherIcon from "./WeatherIcon.js";
 import axios from "axios";
 
 export default function WeatherForWeek(props){
-//     const [forecast, setForecast] = useState("");
+    const [forecast, setForecast] = useState("");
 
 
-// function day(){
-//     let date = new Date(forecast[0].data.dt * 1000);
+// function showDay(){
+//     let date = new Date(forecast[0].dt);
 //     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 //     let day = date.getDay();
+//     let forecastDay = days[day];
 
-//    return days[day];
+//    return forecastDay;
 // }
 
 
  function showResponse (response){
-    // setForecast(response.data.daily);
-    console.log(response.data);
+ setForecast(response.data.daily);
+    console.log(response.data.daily[0].dt*1000);
  }
 
 let lat = props.coord.lat;
@@ -35,16 +36,16 @@ axios.get(apiUrl).then(showResponse);
             <div className="container">
                 <div className="row m-3">
                     <div className="col-2 border-0">
-                        {/* <span>{day()}</span> */}
+                        {/* <span>{showDay}</span> */}
                         <div>
                         <WeatherIcon code={props.data.icon} size={29}/>
                         </div>
                         <span className="forecast-temp">
-                            {/* {Math.round(forecast[0].temp.max)}° */}
+                            {Math.round(forecast[0].temp.max)}°
                             </span>
                         <br />
                         <span className="forecast-temp">
-                            {/* {Math.round(forecast[0].temp.min)}° */}
+                            {Math.round(forecast[0].temp.min)}°
                             </span>
                     </div>
                     
