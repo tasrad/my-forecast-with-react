5a63a9374357abd55cb40a1c0d137d88f95110e9
tasrad <108927043+tasrad@users.noreply.github.com>
2022-11-15 14:05:09 +0200

    created forecast for 6 days

---
+++ src/DailyForecast.js
@@ -0,0 +1,37 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon.js";
+import "./WeatherForWeek.css";
+
+export default function dailyForecast(props){
+    function tempDay(){
+        let dayTemp = Math.round(props.data.temp.day);
+        return `${dayTemp}°`
+    };
+    function tempNight(){
+        let nightTemp = Math.round(props.data.temp.night);
+        return `${nightTemp}°`
+    }
+    function showDay(){
+        let date = new Date(props.data.dt*1000);
+        let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        let day = date.getDay();
+        let forecastDay = days[day];
+       return forecastDay;
+    }
+
+    return(
+        <div className="dailyForecast">
+        <span>{showDay()}</span>
+        <div>
+        <WeatherIcon code={props.data.weather[0].icon} size={29}/>
+        </div>
+        <span className="forecast-temp">
+            {tempDay()}
+            </span>
+        <br />
+        <span className="forecast-temp">
+            {tempNight()}
+            </span>
+   </div>
+    )
+}
--- src/WeatherForWeek.js
+++ src/WeatherForWeek.js
@@ -2,55 +2,47 @@ import React
 , { useState } 
 from "react";
 import "./WeatherForWeek.css";
-import WeatherIcon from "./WeatherIcon.js";
+import DailyForecast from "./DailyForecast.js";
 import axios from "axios";
 
 export default function WeatherForWeek(props){
     const [forecast, setForecast] = useState({});
-    const [dayTemp, setDayTemp] = useState("");
-    const [nightTemp, setNightTemp] = useState("");
+    const [loaded, setLoaded] = useState(false);
+
 
  function showResponse (response){
+    setLoaded(true);
  setForecast(response.data.daily);
- setDayTemp(response.data.daily[0].temp.day);
- setNightTemp(response.data.daily[0].temp.night);
+
  };
 
-let lat = props.coord.lat;
-let lon = props.coord.lon;
-let key = "57821c3b75b60c68ecd1a8d0dd1aa8d3";
-let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=metric`;
-axios.get(apiUrl).then(showResponse);
 
 
-function showDay(){
-    let date = new Date(forecast[0].dt*1000);
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    let day = date.getDay();
-    let forecastDay = days[day];
-   return forecastDay;
-}
 
+
+if (loaded){
     return(
         <div className="WeatherForWeek ">
-            <div className="container">
                 <div className="row m-3">
-                    <div className="col-2 ">
-                        <span>{showDay()}</span>
-                        <div>
-                        <WeatherIcon code={forecast[0].weather[0].icon} size={29}/>
-                        </div>
-                        <span className="forecast-temp">
-                            {Math.round(nightTemp)}°
-                            </span>
-                        <br />
-                        <span className="forecast-temp">
-                            {Math.round(dayTemp)}°
-                            </span>
-                    </div>
+                    {forecast.map( function(dailyForecast, index){
+                        if (index < 6){
+                            return(
+                                <div className="col-2" key={index}>
+                                    <DailyForecast data={dailyForecast} />
+                                 </div>
+                            )
+                        }
+                    })}
                     
                 </div>
-            </div>
         </div>
-    )
+    )} else {
+        let lat = props.coord.lat;
+        let lon = props.coord.lon;
+        let key = "445905dadb3d2b0c6f1b916c9d0e3860";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=metric`;
+        axios.get(apiUrl).then(showResponse);
+
+        return null;
+    }
 }
